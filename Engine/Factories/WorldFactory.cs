2b34a9192749b3cc22de242c907413cfa3ed861b
Ryan Silva <RyandotSilva@gmail.com>
2019-09-28 10:32:45 -0400

    Add CreateWorld method

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Models;
 
 namespace Engine.Factories
 {
     internal class WorldFactory
     {
+        internal World CreateWorld()
+        {
+            return new World();
+        }
     }
 }
