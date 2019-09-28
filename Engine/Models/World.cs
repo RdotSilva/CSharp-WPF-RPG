ad719a0ce3adfceef7bf5d473176e6d2a9f784f5
Ryan Silva <RyandotSilva@gmail.com>
2019-09-28 10:38:09 -0400

    Add AddLocation method

--- Engine/Models/World.cs
+++ Engine/Models/World.cs
@@ -8,5 +8,18 @@ namespace Engine.Models
 {
     public class World
     {
+        private List<Location> _locations = new List<Location>();
+
+        internal void AddLocation(int xCoordinate, int yCoordinate, string name, string description, string imageName)
+        {
+            Location loc = new Location();
+            loc.XCoordinate = xCoordinate;
+            loc.YCoordinate = yCoordinate;
+            loc.Name = name;
+            loc.Description = description;
+            loc.ImageName = imageName;
+
+            _locations.Add(loc);
+        }
     }
 }
