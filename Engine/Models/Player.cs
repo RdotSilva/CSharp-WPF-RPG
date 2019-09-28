a28479114bde1ab47531f209716234590758250c
Ryan Silva <RyandotSilva@gmail.com>
2019-09-28 09:24:09 -0400

    Add interface to Player class

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Remoting.Activation;
 using System.Text;
@@ -7,14 +8,22 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
+        
         public string Name { get; set; }
         public string CharacterClass { get; set; }
         public int HitPoints { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
         public int Gold { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
     }
 }
