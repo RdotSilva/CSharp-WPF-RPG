ac4fa8d81bfd512d1dc942b8b614d5e5e6393d4b
Ryan Silva <RyandotSilva@gmail.com>
2019-09-27 20:09:35 -0400

    Update CurrentPlayer properties

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -15,7 +15,11 @@ namespace Engine.ViewModels
         {
             CurrentPlayer = new Player();
             CurrentPlayer.Name = "Ryan";
+            CurrentPlayer.CharacterClass = "Fighter";
+            CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
         }
     }
 }
