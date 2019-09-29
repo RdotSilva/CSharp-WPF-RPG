9214578052f1bb5f86d931c853b26ca80eea85ba
Ryan Silva <RyandotSilva@gmail.com>
2019-09-29 10:08:37 -0400

    Add movement methods to change location

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -29,5 +29,23 @@ namespace Engine.ViewModels
 
             CurrentLocation = CurrentWorld.LocationAt(0, -1);
         }
+
+        public void MoveNorth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+        }
+        public void MoveEast()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+        }
+        
+        public void MoveSouth()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+        }
+        public void MoveWest()
+        {
+            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+        }
     }
 }
--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -34,22 +34,19 @@ namespace WPFUI
 
         private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
         {
-
+            _gameSession.MoveNorth();
         }
         private void OnClick_MoveWest(object sender, RoutedEventArgs e)
         {
-
+            _gameSession.MoveWest();
         }
         private void OnClick_MoveEast(object sender, RoutedEventArgs e)
         {
-
+            _gameSession.MoveEast();
         }
         private void OnClick_MoveSouth(object sender, RoutedEventArgs e)
         {
-
+            _gameSession.MoveSouth();
         }
     }
-}
-}
-}
 }
