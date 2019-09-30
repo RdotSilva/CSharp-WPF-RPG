6e5657ab163bebe731639c44c32c44a22f747404
Ryan Silva <RyandotSilva@gmail.com>
2019-09-30 16:44:27 -0400

    Add GameItem constructor

--- Engine/Models/GameItem.cs
+++ Engine/Models/GameItem.cs
@@ -8,5 +8,15 @@ namespace Engine.Models
 {
     public class GameItem
     {
+        public int ItemTypeId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public GameItem(int itemTypeID, string name, int price)
+        {
+            ItemTypeId = itemTypeID;
+            Name = name;
+            Price = price;
+        }
     }
 }
