0736a367c476b9bf79e17ff20f1195ca993a5f66
Ryan Silva <RyandotSilva@gmail.com>
2019-09-30 16:49:40 -0400

    Add Weapon class

---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,18 @@
+﻿namespace Engine.Models
+{
+    public class Weapon
+    {
+        public class Weapon : GameItem
+        {
+            public int MinimumDamage { get; set; } 
+            public int MaximumDamage { get; set; }
+
+            public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamage) : base(itemTypeID, name, price)
+            {
+                MinimumDamage = minDamage;
+                MaximumDamage = maxDamage;
+            }
+        }
+
+    }
+}
