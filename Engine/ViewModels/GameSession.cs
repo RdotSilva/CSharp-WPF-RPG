5fa80078b357a0ad5d4b61116775cefd871ad542
Ryan Silva <RyandotSilva@gmail.com>
2019-09-30 16:26:39 -0400

    Refactor CurrentPlayer

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -60,13 +60,7 @@ namespace Engine.ViewModels
         }
         public GameSession()
         {
-            CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Ryan";
-            CurrentPlayer.CharacterClass = "Fighter";
-            CurrentPlayer.HitPoints = 10;
-            CurrentPlayer.Gold = 1000000;
-            CurrentPlayer.ExperiencePoints = 0;
-            CurrentPlayer.Level = 1;
+            CurrentPlayer = new Player {Name = "Ryan", CharacterClass = "Fighter", HitPoints = 10, Gold = 100000, ExperiencePoints = 0, Level = 1};
 
             WorldFactory factory = new WorldFactory();
             CurrentWorld = factory.CreateWorld();
