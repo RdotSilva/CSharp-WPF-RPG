bc5df2f0a45c4297533ca0fcb883f186f760d03e
Ryan Silva <RyandotSilva@gmail.com>
2019-09-30 17:02:30 -0400

    Add CreateGameItem method

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -19,6 +19,16 @@ namespace Engine.Factories
             _standardGameItems.Add(new Weapon.Weapon(1002, "Rusty Sword", 5, 1, 3));
         }
 
-        
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeId == itemTypeID);
+
+            if (standardItem != null)
+            {
+                return standardItem.Clone();
+            }
+
+            return null;
+        }
     }
 }
