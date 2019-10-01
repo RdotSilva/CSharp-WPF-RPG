f295e6f6fc259bd48b241f65013db943da3123cc
Ryan Silva <RyandotSilva@gmail.com>
2019-10-01 19:52:43 -0400

    Add QuestFactory class

---
+++ Engine/Factories/QuestFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    internal static class QuestFactory
+    {
+        private static readonly List<Quest> _quests = new List<Quest>();
+
+        
+    }
+}
