34a8c57c5d44ca99c9df4c51401a4f156106402c
Ryan Silva <RyandotSilva@gmail.com>
2019-10-01 19:57:07 -0400

    Add QuestFactory constructor

--- Engine/Factories/QuestFactory.cs
+++ Engine/Factories/QuestFactory.cs
@@ -8,6 +8,23 @@ namespace Engine.Factories
     {
         private static readonly List<Quest> _quests = new List<Quest>();
 
-        
+        static QuestFactory()
+        {
+            // Declare the items need to complete the quest, and its reward items
+            List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
+            List<ItemQuantity> rewardItems = new List<ItemQuantity>();
+
+            itemsToComplete.Add(new ItemQuantity(9001, 5));
+            rewardItems.Add(new ItemQuantity(1002, 1));
+
+            // Create the quest
+            _quests.Add(new Quest(1,
+                "Clear the herb garden",
+                "Defeat the snakes in the Herbalist's garden",
+                itemsToComplete,
+                25, 10,
+                rewardItems));
+        }
+
     }
 }
