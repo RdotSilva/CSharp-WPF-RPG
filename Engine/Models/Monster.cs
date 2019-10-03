38eb01f76b3449fd01038b31dc35026985c75bd3
Ryan Silva <RyandotSilva@gmail.com>
2019-10-03 16:06:51 -0400

    Add Monster properties

--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    class Monster
+    public class Monster : BaseNotificationClass
     {
+        private int _hitPoints;
+
+        public string Name { get; private set; }
+        public string ImageName { get; set; }
+        public Int16 MaximumHitPoints { get; private set; }
+
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            private set
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+        public ObservableCollection<ItemQuantity> Inventory { get; set; }
     }
 }
