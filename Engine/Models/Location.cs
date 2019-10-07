d1a2489c4d261a4659318fd415f5e69b02817ec0
Ryan Silva <RyandotSilva@gmail.com>
2019-10-07 16:08:12 -0400

    Add GetMonster method

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Factories;
 
 namespace Engine.Models
 {
@@ -29,6 +30,38 @@ namespace Engine.Models
                 // This monster is not already at this location, so add it.
                 MonstersHere.Add(new MonsterEncounter(monsterID, chanceOfEncountering));
             }
+        }
+        public Monster GetMonster()
+        {
+            if (!MonstersHere.Any())
+            {
+                return null;
+            }
+
+            // Total the percentages of all monsters at this location.
+            int totalChances = MonstersHere.Sum(m => m.ChanceOfEncountering);
+
+            // Select a random number between 1 and the total (in case the total chances is not 100).
+            int randomNumber = RandomNumberGenerator.NumberBetween(1, totalChances);
+
+            // Loop through the monster list, 
+            // adding the monster's percentage chance of appearing to the runningTotal variable.
+            // When the random number is lower than the runningTotal,
+            // that is the monster to return.
+            int runningTotal = 0;
+
+            foreach (MonsterEncounter monsterEncounter in MonstersHere)
+            {
+                runningTotal += monsterEncounter.ChanceOfEncountering;
+
+                if (randomNumber <= runningTotal)
+                {
+                    return MonsterFactory.GetMonster(monsterEncounter.MonsterID);
+                }
+            }
+
+            // If there was a problem, return the last monster in the list.
+            return MonsterFactory.GetMonster(MonstersHere.Last().MonsterID);
         }
 
     }
