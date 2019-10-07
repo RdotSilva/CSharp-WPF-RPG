e391a69e697a8455fec38f8e522ddc37b47d3b61
Ryan Silva <RyandotSilva@gmail.com>
2019-10-07 16:05:30 -0400

    Add AddMonster method

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -15,6 +15,21 @@ namespace Engine.Models
         public string ImageName { get; set; }
         public List<Quest> QuestAvailableHere { get; set; } = new List<Quest>();
         public List<MonsterEncounter> MonstersHere { get; set; } = new List<MonsterEncounter>();
+        public void AddMonster(int monsterID, int chanceOfEncountering)
+        {
+            if (MonstersHere.Exists(m => m.MonsterID == monsterID))
+            {
+                // This monster has already been added to this location.
+                // So, overwrite the ChanceOfEncountering with the new number.
+                MonstersHere.First(m => m.MonsterID == monsterID)
+                    .ChanceOfEncountering = chanceOfEncountering;
+            }
+            else
+            {
+                // This monster is not already at this location, so add it.
+                MonstersHere.Add(new MonsterEncounter(monsterID, chanceOfEncountering));
+            }
+        }
 
     }
 }
