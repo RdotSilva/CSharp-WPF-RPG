848defdaf2072dc6b72da536a284ad7241ee5550
Ryan Silva <RyandotSilva@gmail.com>
2019-10-07 16:10:54 -0400

    Add monsters to game locations

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -17,6 +17,8 @@ namespace Engine.Factories
                 "There are rows of corn growing here, with giant rats hiding between them.",
                 "/Engine;component/Images/Locations/FarmFields.png");
 
+            newWorld.LocationAt(-2, 1).AddMonster(2, 100);
+
             newWorld.AddLocation(-1, -1, "Farmer's House",
                 "This is the house of your neighbor, Farmer Ted.",
                 "/Engine;component/Images/Locations/Farmhouse.png");
@@ -41,6 +43,8 @@ namespace Engine.Factories
                 "The trees in this forest are covered with spider webs.",
                 "/Engine;component/Images/Locations/SpiderForest.png");
 
+            newWorld.LocationAt(2, 0).AddMonster(3, 100);
+
             newWorld.AddLocation(0, 1, "Herbalist's hut",
                 "You see a small hut, with plants drying from the roof.",
                 "/Engine;component/Images/Locations/HerbalistsHut.png");
@@ -50,6 +54,8 @@ namespace Engine.Factories
             newWorld.AddLocation(0, 2, "Herbalist's garden",
                 "There are many plants here, with snakes hiding behind them.",
                 "/Engine;component/Images/Locations/HerbalistsGarden.png");
+
+            newWorld.LocationAt(0, 2).AddMonster(1, 100);
 
             return newWorld;
         }
