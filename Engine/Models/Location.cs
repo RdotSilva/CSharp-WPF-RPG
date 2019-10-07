c13bbccd208db97814c1ae91c0daa8af01136128
Ryan Silva <RyandotSilva@gmail.com>
2019-10-07 16:03:57 -0400

    Add MonstersHere property

--- Engine/Models/Location.cs
+++ Engine/Models/Location.cs
@@ -14,6 +14,7 @@ namespace Engine.Models
         public string Description { get; set; }
         public string ImageName { get; set; }
         public List<Quest> QuestAvailableHere { get; set; } = new List<Quest>();
+        public List<MonsterEncounter> MonstersHere { get; set; } = new List<MonsterEncounter>();
 
     }
 }
