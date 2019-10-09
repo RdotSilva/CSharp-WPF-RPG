674f64ebaeba582a08a4fcfa42a79ed65d24284c
Ryan Silva <RyandotSilva@gmail.com>
2019-10-09 15:19:37 -0400

    Add OnClick_AttackMonster event

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -52,6 +52,10 @@ namespace WPFUI
             _gameSession.MoveSouth();
         }
 
+        private void OnClick_AttackMonster(object sender, RoutedEventArgs e)
+        {
+            _gameSession.AttackCurrentMonster();
+        }
         private void OnGameMessageRaised(object sender, GameMessageEventArgs e)
         {
             GameMessages.Document.Blocks.Add(new Paragraph(new Run(e.Message)));
