c146ec8a03e423dcc76298b24f4f0cec2b91c068
Ryan Silva <RyandotSilva@gmail.com>
2019-10-10 16:50:53 -0400

    Refactor Monster ImageName property

--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -36,7 +36,7 @@ namespace Engine.Models
             int rewardExperiencePoints, int rewardGold)
         {
             Name = name;
-            ImageName = string.Format("/Engine;component/Images/Monsters/{0}", imageName);
+            ImageName = $"/Engine;component/Images/Monsters/{imageName}";
             MaximumHitPoints = maximumHitPoints;
             HitPoints = hitPoints;
             MinimumDamage = minimumDamage;
