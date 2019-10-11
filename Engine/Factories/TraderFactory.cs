6c8ec1ebb7aa98ec59b7dd07e9f062479c8892db
Ryan Silva <RyandotSilva@gmail.com>
2019-10-11 18:36:22 -0400

    Add TraderFactory constructor

--- Engine/Factories/TraderFactory.cs
+++ Engine/Factories/TraderFactory.cs
@@ -10,5 +10,20 @@ namespace Engine.Factories
     public static class TraderFactory
     {
         private static readonly List<Trader> _traders = new List<Trader>();
+        static TraderFactory()
+        {
+            Trader susan = new Trader("Susan");
+            susan.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+
+            Trader farmerTed = new Trader("Farmer Ted");
+            farmerTed.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+
+            Trader peteTheHerbalist = new Trader("Pete the Herbalist");
+            peteTheHerbalist.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+
+            AddTraderToList(susan);
+            AddTraderToList(farmerTed);
+            AddTraderToList(peteTheHerbalist);
+        }
     }
 }
