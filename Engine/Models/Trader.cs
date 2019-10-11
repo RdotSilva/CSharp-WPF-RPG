b832d223d38fb08805d63aaf39d37938f9f0aff2
Ryan Silva <RyandotSilva@gmail.com>
2019-10-11 18:30:41 -0400

    Add Trader class

---
+++ Engine/Models/Trader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    class Trader : BaseNotificationClass
+    {
+        public string Name { get; set; }
+        public ObservableCollection<GameItem> Inventory { get; set; }
+    }
+}
