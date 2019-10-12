f4f500c4db471ae4a210748c82a2c722cfc780a4
Ryan Silva <RyandotSilva@gmail.com>
2019-10-12 18:24:13 -0400

    Add OnClick_Buy method

--- WPFUI/TradeScreen.xaml.cs
+++ WPFUI/TradeScreen.xaml.cs
@@ -27,7 +27,6 @@ namespace WPFUI
         {
             InitializeComponent();
         }
-
         private void OnClick_Sell(object sender, RoutedEventArgs e)
         {
             GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
@@ -39,6 +38,25 @@ namespace WPFUI
                 Session.CurrentPlayer.RemoveItemFromInventory(item);
             }
         }
+        private void OnClick_Buy(object sender, RoutedEventArgs e)
+        {
+            GameItem item = ((FrameworkElement)sender).DataContext as GameItem;
 
+            if (item != null)
+            {
+                if (Session.CurrentPlayer.Gold >= item.Price)
+                {
+                    Session.CurrentPlayer.Gold -= item.Price;
+                    Session.CurrentTrader.RemoveItemFromInventory(item);
+                    Session.CurrentPlayer.AddItemToInventory(item);
+                }
+                else
+                {
+                    MessageBox.Show("You do not have enough gold");
+                }
+            }
+        }
+
+        
     }
 }
